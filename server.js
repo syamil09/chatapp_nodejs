2eeffa1288cf1b98b423d11c7892d132f6fbbdcd
syamil09 <syamil2502@gmail.com>
2020-12-16 17:59:37 +0700

    upload project

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+connections = [];
+
+server.listen(3000);
+console.log('Server is running...');
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function(socket) {
+
+	connections.push(socket);
+	console.log('Connected : %s sockets is connecting', connections.length);
+
+	// Disconnect
+	socket.on('disconnect', function(data) {
+		connections.splice(connections.indexOf(socket), 1);
+		console.log('Disconnected : %s sockets is disconnect', connections.length);
+	});
+
+	// Send Message from client
+	socket.on('send message', function(data) {
+		// broadcast message again
+		io.sockets.emit('new message', {msg: data});
+	});
+});
